856b7a121c6b801f1a1099df4f486b826d2d0066
Silas White <silas.white@gordon.edu>
2022-01-26 02:18:27 -0500

    Initialized .h, wrote functions to create board

--- project1.cc
+++ project1.cc
@@ -3,4 +3,69 @@ using std::cout;
 using std::cin;
 using std::endl;
 
-enum Organism { NONE, GESTATING, LIVING, DYING };
+#include "project1.h"
+
+Board::Board(int yCoordinates[], int xCoordinates[], int coordinateLength) {
+  for (int i = 0; i < totalRows; i++) {
+    for (int j = 0; j < totalCols; j++) {
+      if (i == 0 || i == totalRows - 1 || j == 0 || j == totalCols - 1) {
+        boardState[i][j] = BORDER;
+      } else {
+        boardState[i][j] = NONE;
+      }
+    }
+  }
+  
+  for (int i = 0; i < coordinateLength; i++) {
+    boardState[yCoordinates[i] + 1][xCoordinates[i] + 1] = LIVING;
+  }
+};
+
+Organism Board::getOrganism(int yCoordinate, int xCoordinate) {
+  return boardState[yCoordinate][xCoordinate];
+};
+
+void Board::setOrganism(int yCoordinate, int xCoordinate, Organism state) {
+  boardState[yCoordinate][xCoordinate] = state;
+};
+
+void printBoard(Board board) {
+  for (int i = 0; i < totalRows; i++) {
+    for (int j = 0; j < totalCols; j++) {
+      if (i == 0 || i == totalRows - 1) {
+        if (j != 0 && j != totalCols - 1) {
+          cout << "-";
+        } else if (j == 0) {
+          cout << "+";
+        } else {
+          cout << "+" << endl;
+        }
+      } else if (j == 0) {
+        if (i != 0 || i != totalRows - 1) {
+          cout << "|";
+        }
+      } else if (j == totalCols - 1) {
+        if (i != 0 || i != totalRows - 1) {
+          cout << "|" << endl;
+        }
+      } else {
+        if (board.getOrganism(i, j)) {
+          cout << "*";
+        } else {
+          cout << " ";
+        }
+      }
+    }
+  }
+};
+
+int main() {
+  int yCoordinates [1];
+  int xCoordinates [1];
+  yCoordinates[0] = 1;
+  xCoordinates[0] = 1;
+  Board board = Board(yCoordinates, xCoordinates, 1);
+  printBoard(board);
+
+  return 0;
+};
---
+++ project1.h
@@ -0,0 +1,36 @@
+// Declares classes and functions to be used for simulating conways
+// game of life for CPS222 Data Structures and Algorithms at Gordon College
+
+// Authors: Silas White nad Gavin Labrec
+
+// WIP
+#ifndef PROJECT1_H_
+#define PROJECT1_H_
+
+#include <iostream>
+#include <string>
+
+using std::string;
+
+enum Organism { NONE, LIVING, BORDER };
+
+static const int activeRows = 18;
+static const int activeCols = 50;
+static const int totalRows = activeRows + 2;
+static const int totalCols = activeCols + 2;
+
+// Holds the information about the board of organisms and is respobsible for simulating generations, counting neighbors,
+// and providing accessors and mutators for the data
+class Board {
+  public:
+    // Constructor
+    Board(int yCoordinates[], int xCoordinates[], int coordinateLength);
+    // ~Board();
+    int countLivingNeighbors(int yCoordinate, int xCoordinate, Board &board);
+    Organism getOrganism(int yCoordinate, int xCoordinate);
+    void setOrganism(int yCoordinate, int xCoordinate, Organism state);
+  private:
+    Organism boardState[20][52];
+};
+
+#endif // PROJECT1_H_
