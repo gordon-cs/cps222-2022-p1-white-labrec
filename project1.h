696af6745a287223f318c6226a9b30baa663e76c
Silas White <silas.j.white@gmail.com>
2022-02-03 16:08:03 -0500

    More .h comments

--- project1.h
+++ project1.h
@@ -20,11 +20,18 @@ static const int totalRows = activeRows + 2;
 static const int totalCols = activeCols + 2;
 static const char ESC = 27;
 
-// Holds the information about the board of organisms and is respobsible for simulating generations, counting neighbors,
-// and providing accessors and mutators for the data
+
+// Creates the board for Conway's Game of Life will use to create its simulation
 class Board {
   public:
-    // Constructor for the board
+  
+    // Constructor for the board. Takes yCoordinate and xCoordinate array along with the length of those arrays.
+    // The board will then be constructed with organisms set to living at the coordinates specified by the yCoordinates
+    // and xCoordinates array.
+    //
+    // Example:
+    // Board* board = new Board({ 1, 2 }, {2, 1}, 2);
+    // Which creates a new Board with ogranisms set to living at coordinates 1, 2 and 2, 1
     Board(int yCoordinates[], int xCoordinates[], int coordinateLength);
 
     // Counts the living organisms that are around the cell indexed at yCoordinate
